f8cd2e896f52a5521735a303a7815a6d89a150dd
mukatirohit <rohanmukati2002@gmail.com>
2024-11-20 15:44:53 +0530

    folder-structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author= "Rohit Mukati",
+    author_email= "rohanmukati2002@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+    
+)
